840f0a95625d17ad6e9ae4679621198e566c2c75
Riccardo <riccardo.marotti@gmail.com>
2015-08-07 19:31:05 +0200

    quote server/client

--- echoclient.py
+++ echoclient.py
@@ -1,6 +1,8 @@
 from twisted.internet import reactor, protocol
 
+
 class EchoClient(protocol.Protocol):
+
     def connectionMade(self):
         self.transport.write("Hello, world!")
 
@@ -8,12 +10,16 @@ class EchoClient(protocol.Protocol):
         print "Server said:", data
         self.transport.loseConnection()
 
+
 class EchoFactory(protocol.ClientFactory):
+
     def buildProtocol(self, addr):
         return EchoClient()
+
     def clientConnectionFailed(self, connector, reason):
         print "Connection failed."
         reactor.stop()
+
     def clientConnectionLost(self, connector, reason):
         print "Connection lost."
         reactor.stop()
--- echoserver.py
+++ echoserver.py
@@ -1,10 +1,14 @@
 from twisted.internet import protocol, reactor
 
+
 class Echo(protocol.Protocol):
+
     def dataReceived(self, data):
         self.transport.write(data)
 
+
 class EchoFactory(protocol.Factory):
+
     def buildProtocol(self, addr):
         return Echo()
 
